0d451c28ca9b9d4dd360d35f55ecd5cab3df7a7e
xueyan <1152991535@qq.com>
2020-06-11 13:41:04 +0800

    test https transmition

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -17,7 +17,7 @@ router.get('/', async (req: Request, res: Response) => {
 });
 
 //@TODO
-//Add an endpoint to GET a specific resource by Primary Key
+//Add an endpoint to GET a specific resource by Primary Ke
 router.get('/:id', async (req: Request, res: Response) => {
         let { id } = req.params;
         const item = await FeedItem.findByPk(id);
